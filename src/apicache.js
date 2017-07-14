6c575d3e1558c0716d9e70fc5b6468c09d8ee3eb
Luca Osti <plutonio@gmail.com>
2017-07-14 12:56:54 +0200

    Added prefix for redis

--- src/apicache.js
+++ src/apicache.js
@@ -287,7 +287,7 @@ function ApiCache() {
       req.apicacheTimer = new Date()
 
       // In Express 4.x the url is ambigious based on where a router is mounted.  originalUrl will give the full Url
-      var key = req.originalUrl || req.url
+      var key = 'apicache/'+(req.originalUrl || req.url)
 
       // Remove querystring from key if jsonp option is enabled
       if (globalOptions.jsonp) {
